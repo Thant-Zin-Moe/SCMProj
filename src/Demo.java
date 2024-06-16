acf6f7934ff166fd3a4025e3ab415d4d87198bc4
Thant-Zin-Moe <142769260+Thant-Zin-Moe@users.noreply.github.com>
2024-06-16 11:33:48 +0630

    [Complete] Database Connection

--- src/Demo.java
+++ src/Demo.java
@@ -1,32 +1,59 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=TZMDotNetCore;user=sa;password=sasa@123;trustServerCertificate=true";
 
-        try (Connection con = DriverManager.getConnection(connectionUrl);
-             Statement stmt = con.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM tbl_pizza")) {
+//        try (Connection con = DriverManager.getConnection(connectionUrl);
+//             Statement stmt = con.createStatement();
+//             ResultSet rs = stmt.executeQuery("SELECT * FROM user_tbl")) {
+//
+//            StringBuilder allData = new StringBuilder();
+//
+//            int columnCount = rs.getMetaData().getColumnCount();
+//
+//            while (rs.next()) {
+////                System.out.println(rs.getString(1) + " " +
+////                        rs.getString(2)+" "+ rs.getString(3)
+////                        +" "+rs.getString(4));
+//                for(int i = 1; i<= columnCount; i++){
+//                    allData.append(rs.getString(i)).append("\t");
+//                }
+//                allData.append("\n");
+//            }
+//            System.out.println(allData.toString());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        Scanner input = new Scanner(System.in);
+        String query = "INSERT INTO user_tbl (User_name, User_mobile, User_email, User_address) VALUES (?, ?, ?, ?)";
 
-            StringBuilder allData = new StringBuilder();
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
-            int columnCount = rs.getMetaData().getColumnCount();
+            // Set the values for the placeholders (?)
+            System.out.print("Enter your name : ");
+            String name = input.nextLine();
+            System.out.print("Enter your phone number : ");
+            String phNo = input.nextLine();
+            System.out.print("Enter your email : ");
+            String email = input.nextLine();
+            System.out.print("Enter your address : ");
+            String address = input.nextLine();
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, phNo);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, address);
 
-            while (rs.next()) {
-//                System.out.println(rs.getString(1) + " " +
-//                        rs.getString(2)+" "+ rs.getString(3)
-//                        +" "+rs.getString(4));
-                for(int i = 1; i<= columnCount; i++){
-                    allData.append(rs.getString(i)).append("\t");
-                }
-                allData.append("\n");
+            // Execute the insert command
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new row was inserted successfully!");
             }
-            System.out.println(allData.toString());
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
